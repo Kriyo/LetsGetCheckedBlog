bc1a1c467aa3ad4f1a217a8ad24a8803e1066e24
Kriyo <krikenn@gmail.com>
2021-02-26 13:11:28 +0000

    Scaffolded out pages directory in preperation for bringing react-router. Pulled components from App.js out into the home page where it makes sense.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react'
-import { Feature } from './components'
-import { NavContainer } from './containers/navbar'
+import { Home } from './pages'
 
 export default function App() {
   return (
     <>
-      <NavContainer>
-        <Feature>
-          <Feature.Title>Simple Blog</Feature.Title>
-          <Feature.SubTitle>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis praesentium
-          </Feature.SubTitle>
-        </Feature>
-      </NavContainer>
+      <Home />
     </>
   )
 }
---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Feature } from '../components'
+import { NavContainer } from '../containers/navbar'
+
+export default function Home() {
+  return (
+    <>
+      <NavContainer>
+        <Feature>
+          <Feature.Title>Simple Blog</Feature.Title>
+          <Feature.SubTitle>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium
+          </Feature.SubTitle>
+        </Feature>
+      </NavContainer>
+    </>
+  )
+}
