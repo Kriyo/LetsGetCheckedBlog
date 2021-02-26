4a872a6aa6c237adf375ca244190939a5479390a
Kriyo <krikenn@gmail.com>
2021-02-26 13:25:07 +0000

    Refactored: App.js brought in react-router setup initial route for home page and put in placeholders for other routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Home } from './pages'
+import * as ROUTES from './constants/routes'
 
 export default function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+      <Route exact path={ROUTES.BLOG}>
+        <p>Blog page goes here</p>
+      </Route>
+      <Route exact path={ROUTES.ABOUT}>
+        <p>About page goes here</p>
+      </Route>
+      <Route exact path={ROUTES.CONTACT}>
+        <p>Contact page goes here</p>
+      </Route>
+    </Router>
   )
 }
