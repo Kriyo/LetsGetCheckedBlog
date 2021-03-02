02c1e6a2e4acebc230a76447f4e33380ffa6843e
Kriyo <krikenn@gmail.com>
2021-03-02 12:57:03 +0000

    Added builder functions for creating comments and responses to those comments

--- src/pages/blogPost.js
+++ src/pages/blogPost.js
@@ -1,17 +1,64 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { PostsContext } from '../context/posts'
+import { POSTS_ENDPOINT } from '../constants/api'
+import { sortByDate } from '../lib/sortByDate'
 
 import { NavContainer, PostContainer } from '../containers'
 
-const buildBlogPost = (loading, posts, pathname) => {
+// const handleReply = (e) => {
+//   console.log('::> handleReply invoked e: ', e)
+// }
+
+const buildReplyComments = (parentComment, comments) => {
+  let content = null
+
+  content = comments.data.map((comment) => {
+    return comment.parent_id === parentComment.id ? (
+      <p key={`comment=${comment.id}`}>
+        Response Comment: {comment.user} - {comment.date} ---- Parent:
+        {parentComment.user}
+      </p>
+    ) : null
+  })
+  return content
+}
+
+const buildRootComments = (comment) => {
+  return !comment.parent_id ? (
+    <p key={`comment=${comment.id}`}>
+      Parent node: {comment.user} - {comment.date}
+    </p>
+  ) : null
+}
+
+const buildBlogComments = (loading, comments) => {
+  let content = null
+
+  if (!loading && comments) {
+    content = comments.data.map((comment) => {
+      return (
+        <div key={`comment=${comment.id}`}>
+          {buildRootComments(comment)}
+          {buildReplyComments(comment, comments)}
+        </div>
+      )
+    })
+  }
+  return content
+}
+
+// Render the outer comments like so:
+// - root comments
+// - - nested comments with their parent_id
+// Add a Comments component
+// Should associate comments with their parent_id as this comment is a reply to a comment
+
+const buildBlogPost = (loading, posts, currentBlogID) => {
   let content = null
 
   if (!loading && posts) {
     const { blog } = posts
-
-    // Gets the current ID from the URL param and removes the ID from the slug
-    const currentBlogID = pathname.substring(pathname.lastIndexOf('/') + 1)
     const currentBlog = blog.find((post) => post.id === Number(currentBlogID))
 
     content = <PostContainer singlePost posts={[currentBlog]} />
@@ -22,11 +69,31 @@ const buildBlogPost = (loading, posts, pathname) => {
 export const BlogPost = () => {
   const { loading, posts } = useContext(PostsContext)
   const { pathname } = useLocation()
+  const [comments, setComments] = useState(null)
+  // Gets the current ID from the URL param and removes the ID from the slug
+  const currentBlogID = pathname.substring(pathname.lastIndexOf('/') + 1)
+
+  const fetchComments = () => {
+    fetch(`${POSTS_ENDPOINT}/${currentBlogID}/comments`)
+      .then((response) => response.json())
+      .catch((error) => {
+        setComments({ data: null, error })
+      })
+      .then((response) => {
+        const reOrderByDate = sortByDate(response, 'comments')
+        setComments({ data: reOrderByDate, error: null })
+      })
+  }
+
+  useEffect(() => {
+    fetchComments()
+  }, [])
 
   return (
     <>
       <NavContainer />
-      {buildBlogPost(loading, posts, pathname)}
+      {buildBlogPost(loading, posts, currentBlogID)}
+      {buildBlogComments(loading, comments)}
     </>
   )
 }
